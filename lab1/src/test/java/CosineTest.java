09ca3365577ffb57612ceb56aecf6b67485f4f36
Andrey Mileshin <andrej.mileshin@ya.ru>
2018-03-22 21:35:12 +0300

    added lab 1 task 1

--- lab1/src/test/java/CosineTest.java
+++ lab1/src/test/java/CosineTest.java
@@ -10,58 +10,140 @@ import java.util.Map;
  * Created by Andrey on 19.03.2018.
  */
 public class CosineTest extends Assert {
-    private HashMap<Double, Double> arrayTestValues = new java.util.HashMap<>();
-    private HashMap<Double, Double> arrayWrongTestValues = new HashMap<>();
     private static final double DELTA = 0.001;
 
-    @Before
-    public void setUp() {
-        //hashMap, key - table result, value - input value
-        //zero check
-        arrayTestValues.put((double) 1, (double) 0);
+    @Test
+    public void testCosRightPlus() {
+        double expected, actual;
+        String message;
+        // Pi/3
+        expected = (double) 1/2;
+        actual = CosineTaylor.cos(Math.PI /3);
+        message = "Right Plus(0; PI/2)\nEquals: X = " + Math.PI/3 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
 
-        //right side check
-        arrayTestValues.put(Math.sqrt(3) / 2, Math.PI / 6);
-        arrayTestValues.put((double) 0, Math.PI / 2);
-        arrayTestValues.put(-Math.sqrt(2) / 2, 3 * Math.PI / 4);
+        // Pi/4
+        expected = (double) Math.sqrt(2)/2;
+        actual = CosineTaylor.cos(Math.PI /4);
+        message = "Right Plus(0; PI/2)\nEquals: X = " + Math.PI/4 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
 
-        //left side check
-        arrayTestValues.put(Math.sqrt(3) / 2, -Math.PI / 6);
-        arrayTestValues.put((double) 0, -Math.PI / 2);
-        arrayTestValues.put(-Math.sqrt(2) / 2, 3 * -Math.PI / 4);
+        // Pi/6
+        expected = (double) Math.sqrt(3)/2;
+        actual = CosineTaylor.cos(Math.PI /6);
+        message = "Right Plus(0; PI/2)\nEquals: X = " + Math.PI/6 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
 
-        //test coverage boundaries
-        arrayTestValues.put((double) -1, -Math.PI);
-        arrayTestValues.put((double) -1, Math.PI);
-
-        //test NaN, Infinity
-        arrayTestValues.put(Double.NaN, Double.NaN);
-        arrayTestValues.put(Double.NaN, Double.POSITIVE_INFINITY);
-
-        //test values outside the coverage boundaries
-        arrayWrongTestValues.put(Math.sqrt(3) / 2, 7 * Math.PI / 6);
-        arrayWrongTestValues.put(Math.sqrt(3) / 2, -7 * Math.PI / 6);
-    }
-
-    @After
-    public void tearDown() {
-        arrayTestValues.clear();
     }
 
     @Test
-    public void testCos() {
+    public void testCosRightMinus() {
         double expected, actual;
-        for(Map.Entry entry : arrayTestValues.entrySet()) {
-            expected = ( (Double) entry.getKey()).doubleValue();
-            actual = CosineTaylor.cos( ( (Double) entry.getValue() ).doubleValue());
-            String message = "Equals: X = " + entry.getValue() + " Cos(X) = " + entry.getKey();
-            assertEquals(message,expected, actual, DELTA);
-        }
-        for(Map.Entry entry : arrayWrongTestValues.entrySet()) {
-            expected = ( (Double) entry.getKey()).doubleValue();
-            actual = CosineTaylor.cos( ( (Double) entry.getValue() ).doubleValue());
-            String message = "Equals: X = " + entry.getValue() + " Cos(X) = " + entry.getKey();
-            assertNotEquals(message,expected, actual, DELTA);
-        }
+        String message;
+        // Pi*2/3
+        expected = (double) - 1/2;
+        actual = CosineTaylor.cos(2*Math.PI /3);
+        message = "Right Minus(PI/2;PI)\nEquals: X = " + Math.PI/3 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // Pi*3/4
+        expected = (double) -Math.sqrt(2)/2;
+        actual = CosineTaylor.cos(3* Math.PI /4);
+        message = "Right Minus(PI/2;PI)\nEquals: X = " + Math.PI/4 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // Pi*5/6
+        expected = (double) -Math.sqrt(3)/2;
+        actual = CosineTaylor.cos(Math.PI * 5/6);
+        message = "Right Minus(PI/2;PI)\nEquals: X = " + Math.PI/6 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+    }
+
+    @Test
+    public void testCosLeftPlus() {
+        double expected, actual;
+        String message;
+        // -Pi/3
+        expected = (double) 1/2;
+        actual = CosineTaylor.cos(-Math.PI /3);
+        message = "Left Plus(-PI/2;0)\nEquals: X = " + -Math.PI/3 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // -Pi/4
+        expected = (double) Math.sqrt(2)/2;
+        actual = CosineTaylor.cos(-Math.PI /4);
+        message = "Left Plus(-PI/2;0)\nEquals: X = " + Math.PI/4 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // -Pi/6
+        expected = (double) Math.sqrt(3)/2;
+        actual = CosineTaylor.cos(-Math.PI /6);
+        message = "Left Plus(-PI/2;0)\nEquals: X = " + Math.PI/6 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+    }
+
+    @Test
+    public void testCosLeftMinus() {
+        double expected, actual;
+        String message;
+        // -Pi*2/3
+        expected = (double) - 1/2;
+        actual = CosineTaylor.cos(-2*Math.PI /3);
+        message = "Left Minus(-PI;-PI/2)\nEquals: X = " + Math.PI/3 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // -Pi*3/4
+        expected = (double) -Math.sqrt(2)/2;
+        actual = CosineTaylor.cos(-3* Math.PI /4);
+        message = "Left Minus(-PI;-PI/2)\nEquals: X = " + Math.PI/4 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // -Pi*5/6
+        expected = (double) -Math.sqrt(3)/2;
+        actual = CosineTaylor.cos(-Math.PI * 5/6);
+        message = "Left Minus(-PI;-PI/2)\nEquals: X = " + Math.PI/6 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+    }
+
+    @Test
+    public void testCosExtremum() {
+        double expected, actual;
+        String message;
+        // 0
+        expected = (double) 1;
+        actual = CosineTaylor.cos(0);
+        message = "Right Plus(0; PI/2)\nEquals: X = " + 0 + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // Pi
+        expected = (double) -1;
+        actual = CosineTaylor.cos(Math.PI);
+        message = "Right Plus(0; PI/2)\nEquals: X = " + Math.PI + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+        // -Pi
+        expected = (double) -1;
+        actual = CosineTaylor.cos(-Math.PI );
+        message = "Right Plus(0; PI/2)\nEquals: X = " + -Math.PI + "; Cos(X) = " + expected;
+        assertEquals(message,expected, actual, DELTA);
+
+    }
+
+    @Test
+    public void testCosNaNandInfinity() {
+        double expected, actual;
+        String message;
+        // NaN
+        actual = CosineTaylor.cos(Double.NaN);
+        message = "NaN";
+        assertEquals(message,Double.NaN, actual, DELTA);
+
+        // INFINITY
+        actual = CosineTaylor.cos(Double.POSITIVE_INFINITY);
+        message = "INFINITY";
+        assertEquals(message,Double.NaN, actual, DELTA);
     }
 }
